adfb045fc65f90a4fc397470d45ac678fa46a7a0
Rohit singh <raj0eshsing0909@gmail.com>
2024-09-12 04:25:10 +0530

    added the EVENTS file and worked on two events group chats and new user joined

---
+++ backend/Actions.js
@@ -0,0 +1,9 @@
+export const EVENT = {
+  JOIN: "joined",
+  CONNECTED: "connected",
+  ON_CODE_SYNC: "on_code_sync",
+  DISCONNECTED: "disconnected",
+  LEAVE: "leave",
+  JOINED: "joined",
+  GROUP_CHAT: "group_chat"
+}
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import 'dotenv/config'
 import {createServer} from "node:http"
 import {Server} from "socket.io"
 import cors from "cors"
+import {EVENT} from "./Actions.js"
+import { timeStamp } from "node:console"
 
 const app = express();
 const server = createServer(app);
@@ -18,8 +20,60 @@ app.use(cors())
 
 const PORT = process.env.PORT;
 
-// we have to intgrate the users arrays that are joined in the room 
- 
+// we have to intgrate the users arrays that are joined in the room
+// it will return all the array of objects into array
+const userObj = {};
+
+const getAllConnectedUsers = (roomId) => {
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
+    return {
+      socketId,
+      username: userObj[socketId]
+    }
+
+  }) 
+}
+
+io.on("connection", (socket) => {
+  socket.on(EVENT.JOIN, ({roomId, username}) => {
+    userObj[socket.id] = username;
+    const users = getAllConnectedUsers(roomId);
+    console.log("this is users", users)
+    // we can iterate the getAllConnected user array of object
+    users.forEach(({ socketId }) => {
+      io.to(socketId).emit(EVENT.JOINED, {
+        users,
+        username,
+        socketId: socket.id
+      })
+    })
+  })
+  
+  // send message to all the users 
+  socket.on(EVENT.GROUP_CHAT, ({roomId, message}) => {
+    const Username = userObj[socket.id];
+    const chatMessage = {
+      Username,
+      message,
+      timeStamp: new Date()
+    }
+
+    io.in(roomId).emit(EVENT.GROUP_CHAT, chatMessage);
+  })
+
+  // on code sync
+
+  // on disconnecting
+
+  //on vedio call 
+
+  //sync code for newly joined user 
+})
+
+
+
+
+
  io.on("connection", (socket) => {
   console.log("User id is", socket.id);
   socket.emit('welcome', `Welcome to the chat server${socket.id}`)
